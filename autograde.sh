41a56ec1d6be4fec187b87dc6b8b3c6062da8cf4
silviana amethyst <1388063+ofloveandhate@users.noreply.github.com>
2024-03-31 16:15:48 -0500

    made hardcoded things arguments to script, plus added skip for pre-post

--- autograde.sh
+++ autograde.sh
@@ -3,12 +3,17 @@
 # this script assumes that `python` is `python3`.
 # why would it be anything else ;)
 
-# this script must be called with *two* positional arguments:
-# $1: the name of the class, in an exact match as part of 
-# $assignment_num: give the number of the homework to be graded, e.g. either 1 or 4 in autograde.sh DS710 1, or autograde.sh DS710 4a
+# this script *must* be called with *two* named arguments:
+# -r: the name of the class, in an exact match as part of 
+# -a: give the number of the homework to be graded, e.g. either 1 or 4 in autograde.sh DS710 1, or autograde.sh DS710 4a
+#
+# there are several optional arguments:
+# -p: set to anything but 0 to skip the pre-submission tests.  default is 0, so to run the tests.
+# -P: set to anything but 0 to skip the post-submission tests.  default is 0, so to run the tests.
+# -t: set to the time, in whole seconds, for the timeout.  default is 60.
 #
 # also, this script and the code it calls depend on two environment variables being set:
-#   1.  coursenum_REPO_LOC, giving the folder for the git repo for this class
+#   1.  course_number_REPO_LOC, giving the folder for the git repo for this class
 #   2.  CANVAS_CREDENTIAL_FILE , giving the path *and name* of a file containing Python code which defines API_KEY for Canvas.
 
 ################
@@ -19,7 +24,6 @@
 # first, get the directory of this script, because other useful things are in there, too.
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-timeout_duration="60" # number of seconds.  this should probably be an argument to this script.
 
 # https://stackoverflow.com/questions/3474526/stop-on-first-error
 set -e
@@ -29,25 +33,64 @@ command -v timeout >/dev/null 2>&1 || { echo >&2 "'autograde.sh' requires 'timeo
 
 
 
-if [ -z $1 ];
-	then
-		# https://stackoverflow.com/questions/3601515/how-to-check-if-a-variable-is-set-in-bash
-		echo "a first positional argument is required, indicating the course number.  e.g. the 'DS710' or 'DS150' in 'autograde.sh DS710 1', or 'autograde.sh DS150 4a'";
-		exit 1 # https://stackoverflow.com/questions/1378274/in-a-bash-script-how-can-i-exit-the-entire-script-if-a-certain-condition-occurs
-fi
 
-course_number=$1
-COURSE_REPO_LOC="$1_REPO_LOC"
+timeout_duration="60" # number of seconds. 
+course_number="UNSET_USE_DASH_r"
+assignment_num="UNSET_USE_DASH_a"
+skip_pre=0
+skip_post=0
+
+while getopts ":p:P:a:r:t" opt; do
+  case $opt in
+    p) 
+		skip_pre=1
+    	;;
+    P) 
+		skip_post=1
+    	;;
+    a)
+		assignment_num=$OPTARG
+		;;
+	t)
+		timeout_duration=$OPTARG
+		;;
+	r)
+		course_number=$OPTARG
+		;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    	exit 1
+    ;;
+  esac
+
+  # case $OPTARG in
+  #   -*) echo "Option $opt needs a valid argument"
+  #   exit 1
+  #   ;;
+  # esac
+done
+
+
+
+COURSE_REPO_LOC="${course_number}_REPO_LOC"
 echo "course repo location being used: ${!COURSE_REPO_LOC}"
 
-if [ -z $2 ];
+# https://superuser.com/questions/688882/how-to-test-if-a-variable-is-equal-to-a-number-in-shell
+if [[ "${course_number}" == "UNSET_USE_DASH_r" ]];
 	then
 		# https://stackoverflow.com/questions/3601515/how-to-check-if-a-variable-is-set-in-bash
-		echo "a second positional argument is required, indicating the number of the assignment.  e.g. the '1' or '4' in 'autograde.sh DS710 1', or 'autograde.sh DS150 4a'";
+		echo "use a named argument, with flag -r, to specify the course number.  e.g. the '-r DS710' or '-r DS150' in 'autograde.sh -r DS710  -a 1', or 'autograde.sh -r DS150 -a 4a'.  this is used to build an environment variable name, like 'DS710_REPO_LOC'";
 		exit 1 # https://stackoverflow.com/questions/1378274/in-a-bash-script-how-can-i-exit-the-entire-script-if-a-certain-condition-occurs
 fi
 
-assignment_num=$2
+
+if [[ "${assignment_num}" == "UNSET_USE_DASH_a" ]];
+	then
+		# https://stackoverflow.com/questions/3601515/how-to-check-if-a-variable-is-set-in-bash
+		echo "use a named argument, with flag -a, to specify the assigment number.  e.g. the '-a 1' or '-a 4a' in 'autograde.sh -r DS710 -a 1', or 'autograde.sh -r DS150 -a 4a'";
+		exit 1 # https://stackoverflow.com/questions/1378274/in-a-bash-script-how-can-i-exit-the-entire-script-if-a-certain-condition-occurs
+fi
+
+
 
 if [ -z ${COURSE_REPO_LOC+x} ];
 	then
@@ -63,7 +106,7 @@ fi
 
 if test -n "$(find ./ -maxdepth 1 -name '*.py' -print -quit)"
 then
-    echo "autograding $course_number assignment $assignment_num using autograding files from '$COURSE_REPO_LOC'";
+    echo "autograding '$course_number' assignment '$assignment_num' using autograding files from '$COURSE_REPO_LOC', with timeout '$timeout_duration'";
 else
 	echo "no python files found in your current directory.  are you in the correct directory?"
     exit -42
@@ -97,19 +140,6 @@ function copy_data_files {
 
 
 
-
-
-################
-# run the pre-submission tests for every submitted file
-
-
-# copy the unit test file to this folder.  this is because the test imports the file, and if it's not in the same location, things break.
-presuite=${!COURSE_REPO_LOC}/Lesson_${assignment_num}/test_assignment${assignment_num}.py
-echo "grading using pre-submission unit tests: $presuite"
-
-cp "$presuite" ./
-
-
 # copy the solutions file to this folder.  this is because the unit tests imports the file, and if it's not in the same location, things break.
 solutionsfile=${!COURSE_REPO_LOC}/Lesson_${assignment_num}/assignment${assignment_num}_sol.py
 echo "using instructor solution from file ${solutionsfile}"
@@ -117,95 +147,116 @@ echo "using instructor solution from file ${solutionsfile}"
 cp "$solutionsfile" ./
 
 
-# make a directory into which to store the results of running the tests
-if [ ! -d ./_autograding/pre_submission_results ]; then
-	rm -rf ./_autograding/pre_submission_results
-    mkdir -p ./_autograding/pre_submission_results
-fi
-
-set +e
-# $() needed in case filenames have spaces.  weird characters still likely to break this.
 
 
-OIFS="$IFS"
-IFS=$'\n'
+if [[ "$skip_pre" -eq 0 ]]; then
 
-for filename in `find . -type f -name "*.py"`; do
-	# about the 2>, see https://stackoverflow.com/questions/14246119/python-how-can-i-redirect-the-output-of-unittest-obvious-solution-doesnt-wor/22710204
+	################
+	# run the pre-submission tests for every submitted file
 
-	if [[ "$filename" != *"test_assignment${assignment_num}"*".py" ]] && [[ "$filename" != *"sol.py" ]]; then
-	  echo "$filename"
-	  if [[ "$filename" != *"assignment${assignment_num}"*".py" ]]; then
-	  	echo "incorrectly named submission, $filename"
-	  	continue
-	  fi
 
-	  copy_data_files
+	# copy the unit test file to this folder.  this is because the test imports the file, and if it's not in the same location, things break.
+	presuite=${!COURSE_REPO_LOC}/Lesson_${assignment_num}/test_assignment${assignment_num}.py
+	echo "grading using pre-submission unit tests: $presuite"
 
-	  # using SIGINT so that we get a nice traceback
-	  # using --full-trace so that the student / we get a stack trace to find what was running when it was killed.
-	  timeout  --signal=SIGINT --foreground "$timeout_duration"s pytest --full-trace --junitxml=./_autograding/pre_submission_results/"$filename".xml "test_assignment${assignment_num}.py" "$filename" 1> ./_autograding/pre_submission_results/"$filename"_their_output.out
-	  # timeout returns 124 if the command timed out. 
-	  timeout_status=$?
+	cp "$presuite" ./
 
-	  if [[ $timeout_status -eq 124 ]]; then
-	  	message="🐢 testing $filename with test_assignment${assignment_num}.py timed out after $timeout_duration seconds, and consequently generated no test results.  automatically generated result containing one failing test written instead."
-	  	echo $message
-	  	echo $message >> ./_autograding/pre_submission_results/"$filename"_their_output.out 
-	  	python3 "${SCRIPT_DIR}"/make_timeout_xml.py ./_autograding/pre_submission_results/"$filename".xml $timeout_duration
-	  fi
 
+	# make a directory into which to store the results of running the tests
+	if [ ! -d ./_autograding/pre_submission_results ]; then
+		rm -rf ./_autograding/pre_submission_results
+	    mkdir -p ./_autograding/pre_submission_results
 	fi
-done #< <(find . -maxdepth 1 -type d -print0)
 
-# remove empty files
-find ./_autograding/pre_submission_results/ -name "*output.out" -size  0  -delete
+	set +e
+	# $() needed in case filenames have spaces.  weird characters still likely to break this.
 
 
+	OIFS="$IFS"
+	IFS=$'\n'
+
+	for filename in `find . -type f -name "*.py"`; do
+		# about the 2>, see https://stackoverflow.com/questions/14246119/python-how-can-i-redirect-the-output-of-unittest-obvious-solution-doesnt-wor/22710204
+
+		if [[ "$filename" != *"test_assignment${assignment_num}"*".py" ]] && [[ "$filename" != *"sol.py" ]]; then
+		  echo "$filename"
+		  if [[ "$filename" != *"assignment${assignment_num}"*".py" ]]; then
+		  	echo "incorrectly named submission, $filename"
+		  	continue
+		  fi
+
+		  copy_data_files
+
+		  # using SIGINT so that we get a nice traceback
+		  # using --full-trace so that the student / we get a stack trace to find what was running when it was killed.
+		  timeout  --signal=SIGINT --foreground "$timeout_duration"s pytest --full-trace --junitxml=./_autograding/pre_submission_results/"$filename".xml "test_assignment${assignment_num}.py" "$filename" 1> ./_autograding/pre_submission_results/"$filename"_their_output.out
+		  # timeout returns 124 if the command timed out. 
+		  timeout_status=$?
+
+		  if [[ $timeout_status -eq 124 ]]; then
+		  	message="🐢 testing $filename with test_assignment${assignment_num}.py timed out after $timeout_duration seconds, and consequently generated no test results.  automatically generated result containing one failing test written instead."
+		  	echo $message
+		  	echo $message >> ./_autograding/pre_submission_results/"$filename"_their_output.out 
+		  	python3 "${SCRIPT_DIR}"/make_timeout_xml.py ./_autograding/pre_submission_results/"$filename".xml $timeout_duration
+		  fi
+
+		fi
+	done #< <(find . -maxdepth 1 -type d -print0)
+
+	# remove empty files
+	find ./_autograding/pre_submission_results/ -name "*output.out" -size  0  -delete
+
+else
+	echo "skipping pre-submission tests, per your request from the -p argument to this script"
+fi # re: if skip_pre
 
 
 set -e
 ################
-# run the post-submission unit tests for every submitted file
-postsuite="${!COURSE_REPO_LOC}/Lesson_${assignment_num}/test_assignment${assignment_num}_postsubmission.py"
-echo "grading using post-submission unit tests: $postsuite"
-cp "$postsuite" ./
 
-if [ ! -d ./_autograding/post_submission_results ]; then
-    mkdir -p ./_autograding/post_submission_results
-fi
+if [[ "$skip_post" -eq 0 ]]; then
+	# run the post-submission unit tests for every submitted file
+	postsuite="${!COURSE_REPO_LOC}/Lesson_${assignment_num}/test_assignment${assignment_num}_postsubmission.py"
+	echo "grading using post-submission unit tests: $postsuite"
+	cp "$postsuite" ./
 
-set +e
-for filename in `find . -type f -name "*.py"`; do
-	if [[ "$filename" != *"test_assignment${assignment_num}"*".py" ]] && [[ "$filename" != *"sol.py" ]]; then
-	  echo "$filename"
-	  if [[ "$filename" != *"assignment${assignment_num}"*".py" ]]; then
-	  	echo "incorrectly named submission, $filename"
-	  	continue
-	  fi
-	  copy_data_files
-
-	  # using SIGINT so that we get a nice traceback
-	  # using --full-trace so that the student / we get a stack trace to find what was running when it was killed.
-	  timeout  --signal=SIGINT --foreground "$timeout_duration"s pytest --full-trace --junitxml=./_autograding/post_submission_results/"$filename".xml "test_assignment${assignment_num}_postsubmission.py" "$filename" 1> ./_autograding/post_submission_results/"$filename"_their_output.out
-	  # timeout returns 124 if the command timed out.  
-	  # see https://stackoverflow.com/questions/38534097/bash-if-command-timeout-execute-something-else
-	  timeout_status=$?
-
-	  if [[ $timeout_status -eq 124 ]]; then
-	  	message="🐢 testing $filename with test_assignment${assignment_num}_postsubmission.py timed out after $timeout_duration seconds, and consequently generated no test results.  automatically generated result containing one failing test written instead."
-	  	echo $message
-	  	echo $message >> ./_autograding/post_submission_results/"$filename"_their_output.out 
-	  	python3 "${SCRIPT_DIR}"/make_timeout_xml.py ./_autograding/post_submission_results/"$filename".xml $timeout_duration
-	  fi
-
+	if [ ! -d ./_autograding/post_submission_results ]; then
+	    mkdir -p ./_autograding/post_submission_results
 	fi
 
-done;
+	set +e
+	for filename in `find . -type f -name "*.py"`; do
+		if [[ "$filename" != *"test_assignment${assignment_num}"*".py" ]] && [[ "$filename" != *"sol.py" ]]; then
+		  echo "$filename"
+		  if [[ "$filename" != *"assignment${assignment_num}"*".py" ]]; then
+		  	echo "incorrectly named submission, $filename"
+		  	continue
+		  fi
+		  copy_data_files
 
-# remove empty files
-find ./_autograding/post_submission_results/ -name "*output.out" -size  0  -delete
+		  # using SIGINT so that we get a nice traceback
+		  # using --full-trace so that the student / we get a stack trace to find what was running when it was killed.
+		  timeout  --signal=SIGINT --foreground "$timeout_duration"s pytest --full-trace --junitxml=./_autograding/post_submission_results/"$filename".xml "test_assignment${assignment_num}_postsubmission.py" "$filename" 1> ./_autograding/post_submission_results/"$filename"_their_output.out
+		  # timeout returns 124 if the command timed out.  
+		  # see https://stackoverflow.com/questions/38534097/bash-if-command-timeout-execute-something-else
+		  timeout_status=$?
 
+		  if [[ $timeout_status -eq 124 ]]; then
+		  	message="🐢 testing $filename with test_assignment${assignment_num}_postsubmission.py timed out after $timeout_duration seconds, and consequently generated no test results.  automatically generated result containing one failing test written instead."
+		  	echo $message
+		  	echo $message >> ./_autograding/post_submission_results/"$filename"_their_output.out 
+		  	python3 "${SCRIPT_DIR}"/make_timeout_xml.py ./_autograding/post_submission_results/"$filename".xml $timeout_duration
+		  fi
+
+		fi
+
+	done;
+
+	# remove empty files
+	find ./_autograding/post_submission_results/ -name "*output.out" -size  0  -delete
+else
+	echo "skipping post-submission tests, per your request from the -P argument to this script"
+fi
 
 set -e
 # i used to move the unit tests and solution into _autograding, but removed it.
